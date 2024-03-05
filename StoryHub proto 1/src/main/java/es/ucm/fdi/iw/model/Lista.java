2fd06cd098f36b04646c61bc6a5627b1c3b58d58
jbroto <jbroto@ucm.es>
2024-03-05 14:05:50 +0100

    Update Lista.java

--- StoryHub proto 1/src/main/java/es/ucm/fdi/iw/model/Lista.java
+++ StoryHub proto 1/src/main/java/es/ucm/fdi/iw/model/Lista.java
@@ -1,5 +1,58 @@
 package es.ucm.fdi.iw.model;
 
-public class Lista {
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+public class Lista implements Transferable<Lista.Transfer> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
+    @SequenceGenerator(name = "gen", sequenceName = "gen")
+	private long id;
+
+    @ManyToOne(targetEntity = User.class)
+    @Column(nullable = false)
+    private User author;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(targetEntity = Media.class)
+    List<Media> medias;
+
+    private Boolean isPublic;
+    private String categories;
+
+    @Getter
+    @AllArgsConstructor
+    public class Transfer {
+        private String author;
+        private String listName;
+        private Boolean isPublic;
+        private String categories;
+        private List<Media> medias;
+        private long id;
+
+        public Transfer(Lista l){
+            this.author = l.getAuthor().getUsername();
+            this.listName = l.getName();
+            this.isPublic = l.getIsPublic();
+            this.categories = l.getCategories();
+            this.medias = l.getMedias();
+            this.id = l.getId();
+        }
+    }
+
+    @Override
+    public Transfer toTransfer() {
+        return new Transfer(author.getUsername(), name, isPublic, categories, medias, id);
+    }
     
 }
